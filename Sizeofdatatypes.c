091ef9112c48aff1ef3f219713dc04b1bcf27ba0
Shubh3819 <126344166+Shubh3819@users.noreply.github.com>
2025-01-13 00:29:16 +0530

    Create Sizeofdatatypes.c

---
+++ Sizeofdatatypes.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Size of Char:%ld\n",sizeof(char));
+    printf("Size of Intr:%ld\n",sizeof(int));
+    printf("Size of Float:%ld\n",sizeof(float));
+    printf("Size of double:%ld\n",sizeof(double));
+    printf("Size of your name:%ld\n",sizeof("Subhrangshu"));
+    return 0;
+}
