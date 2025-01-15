289952c5d1b4959b7bd582db8fed5313338c26bd
Shubh3819 <126344166+Shubh3819@users.noreply.github.com>
2025-01-15 23:58:18 +0530

    Create Menu-based-doublell.c

---
+++ Menu-based-doublell.c
@@ -0,0 +1,13 @@
+/*Write a program to create a double linked list and perform the following menu based operations on it:
+i. insert an element at specific position 
+ii. delete an element from specific position
+iii. Traverse the list
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    
+}
